99a013d5c3a02560e8a6c8bee2776ef3dbb96274
Krishna Mohan Danda <3972410+iamkrishnamohan@users.noreply.github.com>
2023-01-03 08:52:56 +0530

    API key

--- app/src/main/java/in/newtel/newsapp/utils/Constants.kt
+++ app/src/main/java/in/newtel/newsapp/utils/Constants.kt
@@ -1,7 +1,7 @@
 package `in`.newtel.newsapp.utils
 
 object Constants {
-    const val API_KEY= "ef087d6257c9460dad73b04850519084";
+    const val API_KEY= "";  //Register API key
     const val BASE_URL = "https://newsapi.org"
     const val SEARCH_NEWS_TIME_DELAY = 500L
     const val QUERY_PAGE_SIZE = 20
